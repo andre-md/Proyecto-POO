cc47b8ab3c9db8d7fba53c22d31c1b45baa92e1b
Carlos Andre <carlosandre030961@gmail.com>
2025-11-26 15:51:32 -0500

    First commit

---
+++ src/main/java/Clases/SISTEMADEGESTIONDECANCHAS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Clases;
+
+/**
+ *
+ * @author carlo
+ */
+public class SISTEMADEGESTIONDECANCHAS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
