0652112c85d3841494d710389e76b2509d540008
Carlos Andre <carlosandre030961@gmail.com>
2025-11-26 18:08:09 -0500

    Creacion de clases Pago y Reservas

---
+++ src/main/java/Clases/Pago.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author carlo
+ */
+public class Pago {
+    private int idpago;
+    private String idreserva;
+    private double monto;
+    private String metodoPago;
+    private LocalDate fechapago;
+    private String estado;
+    private String observacion;
+
+    public Pago(int idpago, String idreserva, double monto, String metodoPago, LocalDate fechapago, String estado, String observacion) {
+        this.idpago = idpago;
+        this.idreserva = idreserva;
+        this.monto = monto;
+        this.metodoPago = metodoPago;
+        this.fechapago = fechapago;
+        this.estado = estado;
+        this.observacion = observacion;
+    }
+
+    public int getIdpago() {
+        return idpago;
+    }
+
+    public void setIdpago(int idpago) {
+        this.idpago = idpago;
+    }
+
+    public String getIdreserva() {
+        return idreserva;
+    }
+
+    public void setIdreserva(String idreserva) {
+        this.idreserva = idreserva;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(String metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public LocalDate getFechapago() {
+        return fechapago;
+    }
+
+    public void setFechapago(LocalDate fechapago) {
+        this.fechapago = fechapago;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+    
+    public String toString() {
+        return "Pago: " +
+                "ID=" + idpago +
+                ", Reserva=" + idreserva +
+                ", Monto=" + monto +
+                ", Método=" + metodoPago +
+                ", Fecha=" + fechapago +
+                ", Estado=" + estado +
+                ", Observaciones=" + observacion + "\n";
+    }
+}
--- src/main/java/Clases/Reservas.java
+++ src/main/java/Clases/Reservas.java
@@ -8,8 +8,89 @@ import java.time.LocalDateTime;
 
 
 public class Reservas {
-    private String codigo;
+    private String idreserva;
     private String dni;
+    private String idcancha;
     private LocalDateTime horainicio; 
     private LocalDateTime horafin; 
+    private double pagototal;
+
+    public Reservas(String idreserva, String dni, String idcancha, LocalDateTime horainicio, LocalDateTime horafin, double pagototal) {
+        this.idreserva = idreserva;
+        this.dni = dni;
+        this.idcancha = idcancha;
+        this.horainicio = horainicio;
+        this.horafin = horafin;
+        this.pagototal = pagototal;
+    }
+
+    public String getIdreserva() {
+        return idreserva;
+    }
+
+    public void setIdreserva(String idreserva) {
+        this.idreserva = idreserva;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getIdcancha() {
+        return idcancha;
+    }
+
+    public void setIdcancha(String idcancha) {
+        this.idcancha = idcancha;
+    }
+
+    public LocalDateTime getHorainicio() {
+        return horainicio;
+    }
+
+    public void setHorainicio(LocalDateTime horainicio) {
+        this.horainicio = horainicio;
+    }
+
+    public LocalDateTime getHorafin() {
+        return horafin;
+    }
+
+    public void setHorafin(LocalDateTime horafin) {
+        this.horafin = horafin;
+    }
+
+    public double getPagototal() {
+        return pagototal;
+    }
+
+    public void setPagototal(double pagototal) {
+        this.pagototal = pagototal;
+    }
+
+   
+   
+    public boolean equals(Object obj){
+        if(this == obj){
+            return true;
+        }
+        if(obj == null || getClass() != obj.getClass()){
+            return false;
+        }
+        Reservas res = (Reservas)obj;
+        return idreserva.equals(res.idreserva);
+    }
+    
+    public String toString(){
+        return "Reserva #: " + idreserva +
+               " Cliente: " + dni + 
+               " Cancha: " + idcancha +
+               " Inicio: " + horainicio +
+               " fin: " + horafin +
+               " Pago Total: " + pagototal;
+    }
 }
